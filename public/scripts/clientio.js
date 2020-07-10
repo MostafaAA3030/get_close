e8d7335e93cabf5dc24be5f3fe795a6625a802c6
Mostafa Amin <ahmadi2016php@gmail.com>
2020-07-10 19:12:58 +0430

    send message in group is working now

--- public/scripts/clientio.js
+++ public/scripts/clientio.js
@@ -7,7 +7,11 @@ client.on('connect', function () {
 client.on('contact_list', function (data) {
   contacts.innerHTML = "";
   for(var x = 0; x < data.length; x++) {
-    makeContactElement(data[x]);
+    if(data[x].type == 1) {
+      makeContactElement(data[x].following, data[x].id, data[x].type);
+    } else if (data[x].type == 2) {
+      makeGroupElement(data[x].following, data[x].id, data[x].type);
+    }
   }
 });
 
@@ -17,7 +21,8 @@ getId("send_btn").addEventListener('click', function (e) {
   var username = user_name;
   client.emit('message', {
     myself: username,
-    other: address,
+    other: address.name,
+    other_type: address.type,
     message: message
   });
   message_input.value = "";
@@ -28,34 +33,63 @@ client.on('self_MSG', function (data) {
 });
 
 client.on('send_MSG', function (data) {
-  if(data.receiver === user_name && data.sender === address) {
-    makeMessageBox(data);
-  } else {
-    var sender_note = getId(data.sender + "_n");
-    var sender_note_val = sender_note.innerText;
-    var new_val = parseInt(sender_note_val) + 1;
-    sender_note.innerText = "";
-    sender_note.innerText = new_val;
-//    sender_note.style.visibilaty = display;
+  if(data.receiver_type == 1) {
+    if(data.receiver === user_name && data.sender === address.name) {
+      makeMessageBox(data);
+    } else {
+      var sender_note = getId(data.sender + "_n");
+      var sender_note_val = sender_note.innerText;
+      var new_val = parseInt(sender_note_val) + 1;
+      sender_note.innerText = "";
+      sender_note.innerText = new_val;
+  //    sender_note.style.visibilaty = display;
+    }
+  } else if (data.receiver_type == 2) {
+    if(address.name == data.receiver) {
+      makeMessageBox(data);
+    } else {
+      var sender_note = getId(data.receiver + "_n");
+      var sender_note_val = sender_note.innerText;
+      var new_val = parseInt(sender_note_val) + 1;
+      sender_note.innerText = "";
+      sender_note.innerText = new_val; 
+    }
   }
 });
 
-function fetchMSG(noti_el, sender) {
-  var noti_val = noti_el.innerText;
-  noti_val = parseInt(noti_val);
+function fetchMSG(name, id, type) {
+  var noti_el;
+  if(type == 1) {
+    noti_el = getId(name + '_n');
+  } else if (type == 2) {
+    noti_el = getId(id + '_n');
+  }
+  noti_val = parseInt(noti_el.innerText);
   
   var order_length = 5;
   
   if(noti_val > 0) {
     order_length = order_length + noti_val;
   }
-  
-  var order = {
-    other: getId(sender).innerText,
-    myself: user_name,
-    order_length: order_length,
-    noti_n: noti_val
-  };
+ if(type == 1) {
+    var order = {
+      other: name,
+      other_id: id,
+      other_type: type,
+      myself: user_name,
+      order_length: order_length,
+      noti_n: noti_val
+    };
+  } else if (type == 2) {
+    var order = {
+      other: id,
+      other_name: name,
+      other_type: type,
+      myself: user_name,
+      order_length: order_length,
+      noti_n: noti_val
+    };
+  }
   
   client.emit('fetchMSG', order);
   
--- public/scripts/helper.js
+++ public/scripts/helper.js
@@ -4,44 +4,82 @@ var name_of_user = getId("name_of_user");
 var content_header = getId("content_header");
 var contacts = getId("contacts");
 
-var address = "";
+var address = {
+  name: "",
+  type: ""
+};
 
 function getId(id) {
   return document.getElementById(id);
 }
 
-function makeContactElement (the_name) {
+function makeContactElement (the_name, the_id, type) {
   var div_el = document.createElement('div');
   div_el.setAttribute('class', 'col-12 div_contact');
-  div_el.setAttribute('id', the_name + "_contact");
+  div_el.setAttribute('id', the_name + "_contact"); // ali_contact
   contacts.appendChild(div_el);
   
   var n_el = document.createElement('div');
-  n_el.setAttribute('id', the_name + "_n");
+  n_el.setAttribute('id', the_name + "_n"); // ali_n
   n_el.setAttribute('class', 'n-msg');
-  n_el.setAttribute('onclick', 'fetchMSG(this,"' + the_name + '")');
+  n_el.setAttribute('onclick', 'fetchMSG("' + the_name + '", "' + 
+  the_id + '", "' + type + '")');
   var n_el_txt = document.createTextNode("0");
   n_el.appendChild(n_el_txt);
   getId(the_name + "_contact").appendChild(n_el);
   
   var a_el = document.createElement('div');
   a_el.setAttribute('class', 'a_contact');
-  a_el.setAttribute('id', the_name);
-  a_el.setAttribute('onclick', 'changeAddressing(this)');
+  a_el.setAttribute('id', the_name); // ali
+  a_el.setAttribute('onclick', 'changeAddressing("' + the_name + '", "' + 
+  the_id + '", "' + type + '")');
   var text_node = document.createTextNode(the_name);
   a_el.appendChild(text_node);
   getId(the_name + "_contact").appendChild(a_el);
+} // ali, 1, 1
+
+function makeGroupElement (group_name, group_id, type) {
+  var div_el = document.createElement('div');
+  div_el.setAttribute('class', 'col-12 div_contact');
+  div_el.setAttribute('id', group_id + "_contact"); // group1_contact
+  contacts.appendChild(div_el);
+  
+  var n_el = document.createElement('div');
+  n_el.setAttribute('id', group_id + "_n"); // group1_n
+  n_el.setAttribute('class', 'n-msg');
+  n_el.setAttribute('onclick', 'fetchMSG("' + group_name + '", "'
+  + group_id + '","' + type + '")');
+  var n_el_txt = document.createTextNode("0");
+  n_el.appendChild(n_el_txt);
+  getId(group_id + "_contact").appendChild(n_el);
+  
+  var a_el = document.createElement('div');
+  a_el.setAttribute('class', 'a_contact');
+  a_el.setAttribute('id', group_id); // group1
+  a_el.setAttribute('onclick', 'changeAddressing("' + group_name + '", "' +
+  group_id + '", "' + type + '")');
+  var text_node = document.createTextNode(group_name); //ahmady
+  a_el.appendChild(text_node);
+  getId(group_id + "_contact").appendChild(a_el);
 }
 
-function changeAddressing (el) {
-  if(message_place.innerHTML != null) {
+function changeAddressing (name, id, type) {
+  if(message_place.innerText != "") {
     message_place.innerHTML = "";
     message_input.value = "";
   }
-   
-  address = el.innerText;
-  content_header.innerText = address;
-  var noti_el = getId(address + "_n");
+  if(type == 1) {
+    address.name = name;
+    address.type = type;
+    content_header.innerText = name;
+    
+  } else if(type == 2) {
+    address.name = id;
+    address.type = type;
+    content_header.innerText = name;
+    
+  }
+  var noti_el = getId(address.name + "_n");
   noti_el.click();
   enableMessageInput();
   message_input.focus();
@@ -65,6 +103,8 @@ function makeMessageBox(data) {
   div_el.appendChild(h_el);
   div_el.appendChild(p_el);
   message_place.appendChild(div_el);
+  
+  scrollToBottom();
 }
 
 function layDownMessages (data, new_messages_index) {
@@ -74,14 +114,19 @@ function layDownMessages (data, new_messages_index) {
     }
     makeMessageBox(data.messages[x]);
   }
+  scrollToBottom();
 }
 
 function newMessagesBox () {
   var div_el = document.createElement("div");
-  div_el.setAttribute('class', 'new-msgs-line');
+  div_el.setAttribute('class', 'col-12 new-msgs-line');
   var txt_node = document.createTextNode("New messages");
   div_el.appendChild(txt_node);
   message_place.appendChild(div_el);
+}
+
+function scrollToBottom () {
+  message_place.scrollTop = message_place.scrollHeight;
 }
 
 function enableMessageInput () {
